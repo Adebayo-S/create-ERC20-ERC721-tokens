f3de2158ae3c61c0316104cdb3d5881b5bc6c1d7
Adebayo Samuel <eladebayoor@gmail.com>
2022-03-20 01:52:12 +0100

    feat: deploy contract configured

--- zuriERC20/migrations/2_deploy_contracts.js
+++ zuriERC20/migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const ZuriERC20 = artifacts.require("ZuriERC20");
 
-module.exports = function (deployer) {
-  deployer.deploy(ZuriERC20, 1000000);
+const totalSupply = 10 ** 6 * 10 ** 5;
+const decimals = 5;
+const name = "Zuri coins";
+const symbol = "ZRC";
+
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(ZuriERC20, totalSupply, decimals, name, symbol, {from: accounts[0]});
 };
